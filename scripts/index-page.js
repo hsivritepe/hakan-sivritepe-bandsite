69b8e80399b95149bba4ea67185a84b505566a7e
hsivritepe <hakan@sivritepe.com>
2023-05-05 08:34:16 -0700

    Adjust the shows list's order and styling

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,31 +1,32 @@
-function displayComment() {
-    comments = [
-        {
-            name: 'Connor Walton',
-            date: '02/17/2021',
-            comment:
-                'This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.',
-        },
-        {
-            name: 'Emilie Beach',
-            date: '01/09/2021',
-            comment:
-                'I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.',
-        },
-        {
-            name: 'Miles Acosta',
-            date: '12/20/2020',
-            comment:
-                "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.",
-        },
-    ];
+comments = [
+    {
+        name: 'Connor Walton',
+        date: '02/17/2021',
+        comment:
+            'This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.',
+    },
+    {
+        name: 'Emilie Beach',
+        date: '01/09/2021',
+        comment:
+            'I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.',
+    },
+    {
+        name: 'Miles Acosta',
+        date: '12/20/2020',
+        comment:
+            "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.",
+    },
+];
 
+function displayComment() {
     oldComments = document.querySelector('.old-comments');
     comments.forEach((comment) => {
         let li = document.createElement('li');
         li.className = 'old-comments__container';
         let img = document.createElement('img');
         img.className = 'old-comments__profile-icon';
+        img.src = '/assets/images/grey-background.jpg';
         let divContent = document.createElement('div');
         divContent.className = 'old-comments__content';
         let divTop = document.createElement('div');
@@ -68,3 +69,22 @@ function setDefaultImage() {
     });
 }
 setDefaultImage();
+
+function addNewComment() {
+    let form = document.querySelector('#new-comment-form');
+    console.log;
+    let todayDate = new Date().toLocaleDateString();
+    form.addEventListener('submit', (event) => {
+        event.preventDefault();
+        let comment = {
+            name: document.querySelector('#formName').value,
+            date: todayDate,
+            comment: document.querySelector('#formComment').value,
+        };
+        comments.unshift(comment);
+        let oldComments = document.querySelector('.old-comments');
+        oldComments.innerHTML = '';
+        displayComment();
+    });
+}
+addNewComment();
